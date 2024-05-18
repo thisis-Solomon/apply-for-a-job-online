e0f84fe21dd2b0d0f25ed1ca529724724d7a5e81
Solomon Njobvu <solomonnjobvu1@gmail.com>
2024-05-18 21:25:35 +0200

    vue router added

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,8 @@
+<script setup>
+import Header from "./components/Header.vue";
+</script>
+
 <template>
   <Header />
-  <main class="bg-light-rayish-cyan--background py-16">
-    <div class="relative container p-2 mx-auto w-[80%]">
-      <FilterTags
-        :filters="filteredJobs"
-        @remove-filter="removeFilteredJobs"
-        @clear-filters="clearFilteredJobs"
-      />
-      <section v-for="job in filteredJobsList" :key="job.id">
-        <JobLists :job="job" @add-to-filter="addPositionToFilter" />
-      </section>
-    </div>
-  </main>
+  <router-view />
 </template>
-
-<script setup>
-import { ref, computed } from "vue";
-import Header from "./components/Header.vue";
-import FilterTags from "./components/FilterTags.vue";
-import JobLists from "./components/JobLists.vue";
-import DUMMY_DATA from "./data.json";
-import { jobFilter } from "./utils/jobFilter";
-
-const jobs = ref(DUMMY_DATA);
-const filteredJobs = ref([]);
-
-function removeFilteredJobs(id) {
-  filteredJobs.value = filteredJobs.value.filter(
-    (filteredJob) => filteredJob !== id
-  );
-}
-
-function addPositionToFilter(jobPosition) {
-  if (
-    !filteredJobs.value.includes(jobPosition) &&
-    filteredJobs.value.length < 5
-  ) {
-    filteredJobs.value.push(jobPosition);
-  }
-}
-
-function clearFilteredJobs() {
-  filteredJobs.value = [];
-}
-
-const filteredJobsList = computed(() =>
-  jobFilter(jobs.value, filteredJobs.value)
-);
-</script>
