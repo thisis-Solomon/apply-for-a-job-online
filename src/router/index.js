71ae124b20e2176d881b4a32cdab044a0a20e83a
Solomon Njobvu <solomonnjobvu1@gmail.com>
2024-05-22 23:28:22 +0200

    login components

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import JobDetails from "../views/JobDetails.vue";
 import PostNewJob from "../views/PostNewJob.vue";
+import Login from "../views/Login.vue";
 
 const routes = [
   {
@@ -18,6 +19,11 @@ const routes = [
     path: "/add-new-job",
     name: "PostNewJob",
     component: PostNewJob,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
   },
 ];
 
---
+++ src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <ContainerLayout>
+    <form>
+      <InputField label="Email" placeholder="Enter registered email" />
+      <InputField label="Password" placeholder="Enter password" />
+      <button
+        class="border px-8 py-2 rounded-md uppercase text-sm font-semibold bg-white shadow-md"
+      >
+        Login
+      </button>
+    </form>
+    <p class="text-sm py-5 text-center">
+      Don't have an account
+      <router-link to="/" class="text-sky-700 underline"
+        >sign up here</router-link
+      >
+    </p>
+  </ContainerLayout>
+</template>
+
+<script setup>
+import ContainerLayout from "../components/ContainerLayout.vue";
+import InputField from "../components/shared/InputField.vue";
+</script>
